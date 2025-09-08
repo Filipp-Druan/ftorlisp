12271a253c903ca635bd71884d440acd4e61059d
Filipp Druan <druanf@bk.ru>
2025-09-08 12:09:07 +0300

    Начал парсер

--- src/parser.zig
+++ src/parser.zig
@@ -2,3 +2,18 @@ const std = @import("std");
 const unicode = std.unicode;
 const code_point = @import("code_point");
 const PropsData = @import("PropsData");
+const root = @import("root.zig");
+
+const Lexer = root.lexer.Lexer;
+const ast = root.ast;
+const AstNode = ast.AstNode;
+
+pub const Parser = struct {
+    lexer: Lexer,
+    allocator: std.mem.Allocator,
+    symbols: *ast.SymbolTable,
+    
+    pub fn next(self: *Parser) *AstNode {
+        
+    }
+};
