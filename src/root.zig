ade3d4dd0dcf59f5d60fed739dd8d25338c9d470
Filipp Druan <druanf@bk.ru>
2025-09-08 12:09:38 +0300

    Добавил ast.zig

--- src/root.zig
+++ src/root.zig
@@ -2,3 +2,4 @@
 //! you are making an executable, the convention is to delete this file and
 //! start with main.zig instead.
 pub const lexer = @import("lexer.zig");
+pub const ast = @import("ast.zig");
